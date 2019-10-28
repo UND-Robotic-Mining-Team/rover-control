d368c3eb48561125a81972b1fdc1a2989e5273e3
Nolan Hovland <nolan.hovland@ndus.edu>
2019-10-28 12:39:28 -0500

    added premake file

---
+++ robo-ui/util/premake4.lua
@@ -0,0 +1,26 @@
+#!lua
+solution"rover-control"
+  configurations{"Debug","Release"}
+
+   files{
+	"../src/*.cpp",
+	"../include/*.h"
+	}
+   includedirs{
+        "../include"
+	}
+project "rover-control"
+	kind"ConsoleApp"
+	language"C++"
+	files{"**.h","**.cpp"}
+
+configuration "Debug"
+   defines {"DEBUG"}
+   flags {"Symbols"}
+   targetdir"../build/Debug"
+
+configuration "Release"
+   defines {"NDEBUG"}
+   flags {"Optimize"}
+   targetdir"../build/Debug"
+
